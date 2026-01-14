0b3866660c1c2b2cfedfa813812f9eec7bc7f7db
mdeval00 <mdeval00@fesb.hr>
2026-01-14 20:07:37 +0100

    Ispravljen 4 zadatak.c

--- 4 zadatak/4 zadatak.c
+++ 4 zadatak/4 zadatak.c
@@ -12,11 +12,13 @@ typedef struct Polynomial {
     Term* head;
 } Polynomial;
 
-Term* create_term(int coeff, int exp) {
+void free_polynomial(Polynomial* polynomial);
+
+Term* create_term(int coeff, int exp, int* error) {
     Term* new_term = (Term*)malloc(sizeof(Term));
     if (new_term == NULL) {
-        printf("Greska pri alokaciji memorije!\n");
-        exit(1);
+        *error = 1;
+        return NULL;
     }
     new_term->coefficient = coeff;
     new_term->exponent = exp;
@@ -24,17 +26,21 @@ Term* create_term(int coeff, int exp) {
     return new_term;
 }
 
-void add_term(Polynomial* polynomial, int coeff, int exp) {
+int add_term(Polynomial* polynomial, int coeff, int exp) {
     if (coeff == 0) {
-        return;
+        return 0;
     }
 
-    Term* new_term = create_term(coeff, exp);
+    int error = 0;
+    Term* new_term = create_term(coeff, exp, &error);
+    if (error) {
+        return 1;
+    }
 
     if (polynomial->head == NULL || polynomial->head->exponent < exp) {
         new_term->next = polynomial->head;
         polynomial->head = new_term;
-        return;
+        return 0;
     }
 
     Term* current = polynomial->head;
@@ -58,7 +64,7 @@ void add_term(Polynomial* polynomial, int coeff, int exp) {
             }
             free(current);
         }
-        return;
+        return 0;
     }
 
     new_term->next = current;
@@ -68,74 +74,107 @@ void add_term(Polynomial* polynomial, int coeff, int exp) {
     else {
         previous->next = new_term;
     }
+
+    return 0;
 }
 
-Polynomial* read_polynomial(FILE* file) {
-    Polynomial* polynomial = (Polynomial*)malloc(sizeof(Polynomial));
-    if (polynomial == NULL) {
-        printf("Greska pri alokaciji memorije!\n");
-        exit(1);
+int read_polynomials_from_file(const char* filename, Polynomial** poly1, Polynomial** poly2) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Greska pri otvaranju datoteke '%s'!\n", filename);
+        return 1;
     }
-    polynomial->head = NULL;
+
+    *poly1 = (Polynomial*)malloc(sizeof(Polynomial));
+    *poly2 = (Polynomial*)malloc(sizeof(Polynomial));
+
+    if (*poly1 == NULL || *poly2 == NULL) {
+        printf("Greska pri alokaciji memorije!\n");
+        fclose(file);
+        free(*poly1);
+        free(*poly2);
+        *poly1 = NULL;
+        *poly2 = NULL;
+        return 1;
+    }
+
+    (*poly1)->head = NULL;
+    (*poly2)->head = NULL;
 
     int coeff, exp;
-    char buffer[100];
+    int reading_first = 1;
+    int error = 0;
 
-    while (1) {
-       
-        if (fgets(buffer, sizeof(buffer), file) == NULL) {
-            break;
+    while (fscanf(file, "%d %d", &coeff, &exp) == 2) {
+        if (reading_first) {
+            error = add_term(*poly1, coeff, exp);
+        }
+        else {
+            error = add_term(*poly2, coeff, exp);
         }
 
-       
-        if (buffer[0] == '\n' || (buffer[0] == '\r' && buffer[1] == '\n')) {
-            break;
+        if (error) {
+            printf("Greska pri obradi polinoma!\n");
+            fclose(file);
+            return 1;
         }
 
-      
-        char* ptr = buffer;
-        int items_read;
-        while (sscanf(ptr, "%d %d%n", &coeff, &exp, &items_read) == 2) {
-            add_term(polynomial, coeff, exp);
-            ptr += items_read;
+        char c = fgetc(file);
+        if (c == '\n') {
+            reading_first = 0;
         }
-
-       
-        if (ptr == buffer) {
-            break;
+        else if (c != EOF) {
+            ungetc(c, file);
         }
     }
 
-    return polynomial;
+    if (ferror(file)) {
+        printf("Greska pri citanju datoteke!\n");
+        fclose(file);
+        return 1;
+    }
+
+    fclose(file);
+    return 0;
 }
-Polynomial* add_polynomials(Polynomial* p1, Polynomial* p2) {
+
+Polynomial* add_polynomials(Polynomial* p1, Polynomial* p2, int* error) {
     Polynomial* result = (Polynomial*)malloc(sizeof(Polynomial));
     if (result == NULL) {
-        printf("Greska pri alokaciji memorije!\n");
-        exit(1);
+        *error = 1;
+        return NULL;
     }
     result->head = NULL;
 
     Term* current = p1->head;
     while (current != NULL) {
-        add_term(result, current->coefficient, current->exponent);
+        if (add_term(result, current->coefficient, current->exponent)) {
+            *error = 1;
+            free_polynomial(result);
+            return NULL;
+        }
         current = current->next;
     }
 
     current = p2->head;
     while (current != NULL) {
-        add_term(result, current->coefficient, current->exponent);
+        if (add_term(result, current->coefficient, current->exponent)) {
+            *error = 1;
+            free_polynomial(result);
+            return NULL;
+        }
         current = current->next;
     }
 
+    *error = 0;
     return result;
 }
 
-Polynomial* multiply_polynomials(Polynomial* p1, Polynomial* p2) {
+Polynomial* multiply_polynomials(Polynomial* p1, Polynomial* p2, int* error) {
     Polynomial* result = (Polynomial*)malloc(sizeof(Polynomial));
     if (result == NULL) {
-        printf("Greska pri alokaciji memorije!\n");
-        exit(1);
+        *error = 1;
+        return NULL;
     }
     result->head = NULL;
 
@@ -147,16 +186,26 @@ Polynomial* multiply_polynomials(Polynomial* p1, Polynomial* p2) {
         while (j != NULL) {
             int new_coeff = i->coefficient * j->coefficient;
             int new_exp = i->exponent + j->exponent;
-            add_term(result, new_coeff, new_exp);
+            if (add_term(result, new_coeff, new_exp)) {
+                *error = 1;
+                free_polynomial(result);
+                return NULL;
+            }
             j = j->next;
         }
         i = i->next;
     }
 
+    *error = 0;
     return result;
 }
 
 void print_polynomial(Polynomial* polynomial) {
+    if (polynomial == NULL) {
+        printf("(NULL)\n");
+        return;
+    }
+
     Term* current = polynomial->head;
 
     if (current == NULL) {
@@ -198,6 +247,10 @@ void print_polynomial(Polynomial* polynomial) {
 }
 
 void free_polynomial(Polynomial* polynomial) {
+    if (polynomial == NULL) {
+        return;
+    }
+
     Term* current = polynomial->head;
     Term* next;
 
@@ -210,23 +263,28 @@ void free_polynomial(Polynomial* polynomial) {
     free(polynomial);
 }
 
+void cleanup(Polynomial* p1, Polynomial* p2, Polynomial* sum, Polynomial* product) {
+    free_polynomial(p1);
+    free_polynomial(p2);
+    free_polynomial(sum);
+    free_polynomial(product);
+}
+
 int main() {
-    FILE* file;
     char filename[100];
+    Polynomial* poly1 = NULL;
+    Polynomial* poly2 = NULL;
+    Polynomial* sum = NULL;
+    Polynomial* product = NULL;
+    int error = 0;
 
     printf("Unesite ime datoteke: ");
     scanf("%s", filename);
 
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Greska pri otvaranju datoteke!\n");
+    if (read_polynomials_from_file(filename, &poly1, &poly2)) {
+        cleanup(poly1, poly2, sum, product);
         return 1;
     }
-
-    Polynomial* poly1 = read_polynomial(file);
-    Polynomial* poly2 = read_polynomial(file);
-
-    fclose(file);
 
     printf("\nPrvi polinom: ");
     print_polynomial(poly1);
@@ -234,18 +292,26 @@ int main() {
     printf("Drugi polinom: ");
     print_polynomial(poly2);
 
-    Polynomial* sum = add_polynomials(poly1, poly2);
+    sum = add_polynomials(poly1, poly2, &error);
+    if (error) {
+        printf("Greska pri zbrajanju polinoma!\n");
+        cleanup(poly1, poly2, sum, product);
+        return 1;
+    }
+
     printf("Zbroj polinoma: ");
     print_polynomial(sum);
 
-    Polynomial* product = multiply_polynomials(poly1, poly2);
+    product = multiply_polynomials(poly1, poly2, &error);
+    if (error) {
+        printf("Greska pri mnozenju polinoma!\n");
+        cleanup(poly1, poly2, sum, product);
+        return 1;
+    }
+
     printf("Umnozak polinoma: ");
     print_polynomial(product);
 
-    free_polynomial(poly1);
-    free_polynomial(poly2);
-    free_polynomial(sum);
-    free_polynomial(product);
-
+    cleanup(poly1, poly2, sum, product);
     return 0;
 }
